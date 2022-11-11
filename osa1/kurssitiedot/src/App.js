dab0e48d0d5d230b29def8f3a7ff17a0e7252aac
Simo <simo.kuparinen@helsinki.fi>
2022-11-11 16:44:01 +0200

    2.1-2.5

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,48 +1,54 @@
-const Header = (props) => {
-  return (
-    <h1>{props.course.name}</h1>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <p>{props.course.parts.map(part => <div>{part.name} {part.exercises}</div>)}</p>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  const total = props.course.parts.map(part => part.exercises).reduce((prev, curr) => prev + curr, 0)
-  return (
-    <div>Number of exercises {total}</div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Course courses={courses} />
     </div>
   )
 }
---
+++ osa1/kurssitiedot/src/components/Course.js
@@ -0,0 +1,33 @@
+const Course = ({courses}) => {
+  return (
+    <>
+      <h2>Web development curriculum</h2>
+      {courses.map(course =>
+        <div key={course.id}>
+          <h3>
+            {course.name}
+          </h3>
+          <div>
+            {course.parts.map(part =>
+              <div key={part.id}>
+                {part.name} {part.exercises}
+              </div>
+            )}
+          </div>
+          <div>
+            <b>
+              total of&nbsp; 
+                {course.parts.map(
+                  part => part.exercises)
+                  .reduce((prev, curr) => prev + curr, 0)
+                }
+              &nbsp;exercises
+            </b>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Course
