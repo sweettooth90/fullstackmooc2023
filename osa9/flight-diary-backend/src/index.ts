d79b12a169798ac9230fc9b10a74628611e578c7
Simo <simo.kuparinen@helsinki.fi>
2023-05-04 00:29:45 +0300

    9.19

---
+++ osa9/flight-diary-backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+const app = express();
+import diaryRouter from './routes/diaries';
+import cors from 'cors';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+
+app.use(express.json());
+
+const PORT = 3001;
+
+app.get('/ping', (_req, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
+});
+
+app.use('/api/diaries', diaryRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
